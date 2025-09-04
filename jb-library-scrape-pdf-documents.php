129173317baf77a33f1572408d0b2038b796ecb8
Jess Boctor <contactme@jessboctor.com>
2025-09-04 15:43:26 -0500

    Create process for scraping PDF data

---
+++ jb-library-scrape-pdf-documents.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * When dealing with thousands of files, we need a way to scrape the PDF text programmatically
+ * 
+ * The goal here is to create a CLI Command that will pull the file name 
+ */
